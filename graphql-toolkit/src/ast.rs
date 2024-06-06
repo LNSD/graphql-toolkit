b2caf5c823484fcf57c5d6b0447584fc00193fdd
Lorenzo Delgado <lnsdev@proton.me>
2024-06-06 15:19:40 +0200

    feat(graphql-toolkit): add into ast trait
    Signed-off-by: Lorenzo Delgado <lnsdev@proton.me>

--- graphql-toolkit/src/ast.rs
+++ graphql-toolkit/src/ast.rs
@@ -1,37 +1,74 @@
-//! This module contains the AST types for the GraphQL query language.
+//! This module contains the AST types and traits for the GraphQL query language.
 
 pub use async_graphql_parser::{types::*, Pos, Positioned};
 pub use async_graphql_value::*;
 
-/// AST extension trait for adding position information to AST nodes.
-pub trait AstPositionExt {
-    /// Create a non-positioned version of this AST node.
-    fn default_position(self) -> Positioned<Self>;
+/// A value-to-AST conversion trait that consumes the input value.
+pub trait IntoAst<T> {
+    /// Convert this value into an AST type.
+    #[must_use]
+    fn into_ast(self) -> T;
+}
 
-    /// Create a positioned version of this AST node.
-    fn with_position(self, pos: Pos) -> Positioned<Self>;
+macro_rules! impl_into_ast {
+    ($($ty:ty),* $(,)?) => {
+        $(
+            impl IntoAst<$ty> for $ty {
+                /// Returns the input value unchanged.
+                #[inline(always)]
+                fn into_ast(self) -> $ty {
+                    self
+                }
+            }
+        )*
+    };
+}
+
+impl<S> IntoAst<Name> for S
+where
+    S: AsRef<str>,
+{
+    #[inline]
+    fn into_ast(self) -> Name {
+        Name::new(self)
+    }
+}
+
+// Alphabetically sorted list of AST types
+impl_into_ast! {
+    BaseType, ConstValue, Directive, DocumentOperations, Field, FragmentDefinition, FragmentSpread,
+    InlineFragment, Number, OperationDefinition, OperationType, Selection, SelectionSet,
+    Type, TypeCondition, Value, VariableDefinition,
+}
+
+/// Extension trait for adding position information to AST nodes.
+pub trait AstPositionExt: Sized {
+    /// Create a positioned version of this AST node with the default position (0:0).
+    #[must_use]
+    #[inline]
+    fn default_position(self) -> Positioned<Self> {
+        Positioned::new(self, Default::default())
+    }
+
+    /// Create a positioned version of this AST node with the given position.
+    #[must_use]
+    #[inline]
+    fn with_position(self, pos: Pos) -> Positioned<Self> {
+        Positioned::new(self, pos)
+    }
 }
 
 macro_rules! impl_ast_position_ext {
     ($($ty:ty),* $(,)?) => {
         $(
-            impl AstPositionExt for $ty {
-                fn default_position(self) -> Positioned<Self> {
-                    Positioned::new(self, Default::default())
-                }
-
-                fn with_position(self, pos: Pos) -> Positioned<Self> {
-                    Positioned::new(self, pos)
-                }
-            }
+            impl AstPositionExt for $ty {}
         )*
     };
-    () => {};
 }
 
 // Alphabetically sorted list of AST types
 impl_ast_position_ext! {
-    BaseType, ConstValue, Directive, DocumentOperations, Field, FragmentDefinition,
+    BaseType, ConstValue, Directive, DocumentOperations, Field, FragmentDefinition, FragmentSpread,
     InlineFragment, Name, Number, OperationDefinition, OperationType, Selection, SelectionSet,
     Type, TypeCondition, Value, VariableDefinition,
 }
