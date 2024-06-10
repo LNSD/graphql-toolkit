860b9245ecebd3de13bbb49f5ad531b62e5f75bd
Lorenzo Delgado <lnsdev@proton.me>
2024-06-10 10:09:41 +0200

    feat(writer): add pretty formatter (#54)
    Signed-off-by: Lorenzo Delgado <lnsdev@proton.me>

--- graphql-toolkit-writer/src/lib.rs
+++ graphql-toolkit-writer/src/lib.rs
@@ -6,5 +6,6 @@ mod ser;
 pub use fmt::{
     compact::{to_string, to_vec, to_writer},
     formatter::Formatter,
+    pretty::{to_string_pretty, to_vec_pretty, to_writer_pretty},
 };
 pub use ser::{Serialize, Serializer};
